9d2c92b4053d8680c4f5b2d8c6760c7ffbebc13c
Taylor Gibb <taygibb@gmail.com>
2014-07-18 09:59:10 +0200

    Initial commit

---
+++ KinectBreaker.Harness/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace KinectBreaker.Harness
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var connection = new HubConnection("http://localhost:53059/");
+            var hub = connection.CreateHubProxy("GameHub");
+            
+            connection.Start().Wait();
+
+            if (args[0] == "/kinect")
+            {
+                //kinect specific code goes here
+            }
+            else
+            {
+                bool die = false;
+
+                while (!die)
+                {
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.Z:
+                            hub.Invoke("Move", -1);
+                            break;
+                        case ConsoleKey.X:
+                            hub.Invoke("Move", 1);
+                            break;
+                        case ConsoleKey.Escape:
+                            die = true;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ KinectBreaker.Web/Scripts/controls.js
@@ -0,0 +1,32 @@
+﻿$(function() {
+
+    var con = $.hubConnection(),
+        hub = con.createHubProxy("kinectGame");
+
+    con.start().done(function() {
+
+        document.onkeydown = function keyboardController(e) {
+            if (!e) {
+                e = window.event;
+            }
+
+            switch (e.keyCode) {
+            case 37:
+                $('#left-hand-side').css('color', 'green');
+                hub.invoke('groupMove', -1);
+
+                break;
+            case 39:
+                $('#right-hand-side').css('color', 'green');
+                hub.invoke('groupMove', 1);
+                break;
+            }
+        };
+        document.onkeyup = function keyboardController(e) {
+            if (!e) {
+                e = window.event;
+            }
+            $('#left-hand-side, #right-hand-side').css('color', '#F03060');
+        };
+    });
+});
